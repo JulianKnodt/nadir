c41c34c18773c67c2bff5876a0ad53844ba8228b
julianknodt <julianknodt@gmail.com>
2018-12-13 20:39:56 -0500

    Started working on stop strategy

--- src/stop.rs
+++ src/stop.rs
@@ -3,14 +3,29 @@ use std::sync::Arc;
 pub enum Strategy {
   Threshold(f32),
   IterCount(u32),
+  // only stop once threshold is below a certain absolute value
+  // dangerous if the function is never below that value.
+  AbsoluteThreshold(f32),
 }
 
 pub(crate) trait StrategyInstance<S> where S: ndarray::Data {
   fn should_stop(&self, image: &S) -> bool;
 }
 
-impl Strategy {
-   pub(crate) fn instance<S: ndarray::Data>(&self) -> Arc<StrategyInstance<S>> {
+struct AbsoluteThreshold(f32);
+
+impl trait StrategyInstance<S> where S: ndarray::Data {
+  fn should_stop(&self, image: &S) -> bool {
     unimplemented!();
+    // upon inpection it appears that ndarray::Data does not have a norm property
+  }
+}
+
+impl Strategy {
+  pub(crate) fn instance<S: ndarray::Data>(&self) -> Arc<StrategyInstance<S>> {
+    match self {
+      Strategy::AbsoluteThreshold(f32) => AbsoluteThreshold(f32),
+      _ => unimplemented!(),
+    }
   }
 }
