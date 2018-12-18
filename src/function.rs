11f4251263398a098564c23862e9117d495f070c
julianknodt <julianknodt@gmail.com>
2018-12-18 02:01:37 -0800

    Added initial hessian matrices
    Issue is now multiplying a column and row vector

--- src/function.rs
+++ src/function.rs
@@ -2,7 +2,11 @@ extern crate ndarray;
 
 use crate::{search, stop};
 
+use std::ops::{Add, Sub, Mul, Div};
 use self::ndarray::Ix1;
+
+extern crate num_traits;
+use self::num_traits::identities::{Zero, One};
 
 /// Type alias of functions that we are expecting.
 pub type Function<A, S> where S: ndarray::Data<Elem=A> =
@@ -21,10 +25,11 @@ pub fn find_min<A,S>(
   stop_strat: stop::Strategy
   ) -> (ndarray::Array<A, Ix1>, A)
   where
-  A: Clone,
+  A: Clone + Mul<A, Output=A> + Add<A, Output=A> + Sub<A, Output=A> + Div<A, Output=A> +
+  One + Zero + Div<f64, Output=A> + PartialOrd + ndarray::ScalarOperand,
   S: ndarray::Data<Elem=A> + Clone + ndarray::DataClone {
   let mut stop_inst = stop_strat.instance();
-  let mut search_inst = search_strat.instance();
+  let mut search_inst = search_strat.instance(start.dim());
   let mut curr = start.to_owned();
   let mut curr_image: A = f(&curr);
   while !stop_inst.should_stop(&curr_image) {
--- src/search.rs
+++ src/search.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use crate::function::{Function, FunctionGradient};
 use crate::line_search::{golden_section_search};
 
-use self::ndarray::{Ix1, Ix2};
+use self::ndarray::{Ix, Ix1, Ix2};
 
 /// The set of possible strategies to pick from
 pub enum Strategy {
@@ -14,9 +14,8 @@ pub enum Strategy {
 
 struct BFGS<A>
   where A: Clone + Add<A, Output=A> + Mul<A, Output=A> + Sub<A, Output=A> {
-  // maintains an internal reference to the hessian approximation
-  // and is initialized to the identity matrix TODO
   hessian_approx: ndarray::Array<A, Ix2>,
+  inverse_approx: ndarray::Array<A, Ix2>,
 }
 
 impl <A, S> StrategyInstance<A, S> for BFGS<A>
@@ -32,22 +31,27 @@ impl <A, S> StrategyInstance<A, S> for BFGS<A>
     // B_k p_k = - grad f(x);
     // I would imagine that in most cases converting this into an LU system and solving would be
     // good, but wikipedia lists an efficient way to perform the inverse, which should be
-    // exploited. TODO
+    // exploited.
     let curr_grad = grad(curr);
-    let direction:ndarray::Array<A,Ix1> = unimplemented!();
+    let direction:ndarray::Array<A,Ix1> = curr_grad * self.inverse_approx;
     let step_size = golden_section_search(f, curr, &direction);
     let step: ndarray::Array<A,Ix1> = direction * step_size;
     let next = curr + &step;
     let update = &next - curr;
     let update_t = update.t().to_owned();
     let step_t = step.t().to_owned();
+    let test = ndarray::ArrayBase::eye(update.dim()) * update;
 
     let hess_approx = &self.hessian_approx;
-    let next_hessian = hess_approx + (update.dot(&update_t))/(update_t * &step)
+    let next_hessian = hess_approx + (update * &update_t)/(update_t * &step)
       - (hess_approx * &step * &step_t * hess_approx)/(&step_t * hess_approx * &step);
     self.hessian_approx = next_hessian;
 
-    next
+    let inv_approx = &self.inverse_approx;
+    let next_inv = unimplemented!();
+    self.inverse_approx = next_inv;
+
+    return next;
   }
 }
 
@@ -62,13 +66,21 @@ pub(crate) trait StrategyInstance <A, S> where S: ndarray::Data<Elem=A> {
   where S: ndarray::Data<Elem=A>;
 }
 
+extern crate num_traits;
+use self::num_traits::identities::{Zero, One};
 impl Strategy {
-   /// returns a specific instance of a strategy so that it can be called in different methods
-   /// concurrently
-   pub(crate) fn instance<A, S>(&self) -> Arc<StrategyInstance<A, S>>
-   where
-   A: Clone,
-   S: ndarray::Data<Elem=A> {
-    unimplemented!();
+  /// returns a specific instance of a strategy so that it can be called in different methods
+  /// concurrently
+  pub(crate) fn instance<A, S>(&self, dim: Ix) -> Arc<StrategyInstance<A, S>>
+  where
+  A: Clone + Mul<A, Output=A> + Add<A, Output=A> + Sub<A, Output=A> + Div<A, Output=A> +
+  One + Zero + Div<f64, Output=A> + PartialOrd + ndarray::ScalarOperand,
+  S: ndarray::Data<Elem=A> {
+    match self {
+      Strategy::BFGS => Arc::new(BFGS{
+        hessian_approx: ndarray::ArrayBase::eye(dim),
+        inverse_approx: ndarray::ArrayBase::eye(dim),
+      }),
+    }
   }
 }
